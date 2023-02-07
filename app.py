0ed0577667ac1460b309f78a6828fd68981b4a03
왕혜민 <whm0413@icraft21.com>
2023-02-07 13:29:10 +0900

    requirements

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello():
     return render_template('index.html', content="hello whm")
 
 if __name__ == '__main__':
-    app.run(host="127.0.0.1",port="8000")
+    app.run(host='0.0.0.0', port=8080)
