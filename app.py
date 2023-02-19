ebcc22a691f72d8d2a663343035b206c7bb978aa
hyeminwang <minasang33@gmail.com>
2023-02-19 23:29:28 +0900

    timer change

--- app.py
+++ app.py
@@ -4,11 +4,7 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from flask import Flask, render_template, redirect, url_for  # Flask 라이브러리 선언
 app = Flask(__name__)
 
-from bs4 import BeautifulSoup
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+import requests
 
 url ="https://coinmarketcap.com/exchanges/upbit/"
 oldList =[]
@@ -26,12 +22,13 @@ top10 = []
 def scheduler():
     print("Scheduler is alive!")
 def printListout(copyArr, preCnt, prevValue):
+    print('preCnt', preCnt, type(preCnt))
     pList = {
-        "id": copyArr[0],
-        "ticker": copyArr[1],
-        "change": preCnt+"->"+copyArr[0],
+        "id": copyArr['idx'],
+        "ticker": copyArr['ticker'],
+        "change": str(preCnt)+"->"+str(copyArr['idx']),
         "prevValue": prevValue,
-        "bw": int(preCnt) - int(copyArr[0])
+        "bw": preCnt - copyArr['idx']
     }
     return pList
 
@@ -39,12 +36,12 @@ def printListout(copyArr, preCnt, prevValue):
 def compareList(oldList, newList):
     global top10
     result = []
-    for n in newList:
-        for o in oldList:
-            if n[1] == o[1]: #ticker 가 같은지 비교
-                arr = printListout(n, o[0], o[6])
+    for i, n in enumerate(newList):
+        for j, o in enumerate(oldList):
+            if n['ticker'] == o['ticker']: #ticker 가 같은지 비교
+                arr = printListout(n, o['idx'], o['volume'])
                 result.append(arr)
-                between = int(n[0]) - int(o[0])
+                between = i - j
                 fArr = []
                 bArr = []
 
@@ -53,11 +50,11 @@ def compareList(oldList, newList):
                 else:
 
                     for x in top10:
-
-                        if x['bw'] >= between:
-                            bArr.append(x.copy())
-                        else:
-                            fArr.append(x.copy())
+                        if between < 0:
+                            if x['bw'] > between:
+                                bArr.append(x.copy())
+                            else:
+                                fArr.append(x.copy())
 
                     if len(fArr) == 0:
                         fArr = [arr.copy()]
@@ -77,81 +74,51 @@ def compareList(oldList, newList):
 
 
 def get_crolling():
-    print('get_crolling start')
+    # print('get_crolling start')
     global oldList
+    url1 = "https://api.upbit.com/v1/market/all"
+    response = requests.get(url1)
+    markets = response.json()
 
-    # Chrome의 경우 | 아까 받은 chromedriver의 위치를 지정해준다.
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument('--headless')
-    chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
-    driver.implicitly_wait(3)
-    # chrome 창을 원하는 가로폭과 세로폭으로 저정합니다.
-    driver.set_window_size(1024, 968)
+    krw_markets = []
+    for market in markets:
+        if market['market'].startswith('KRW'):
+            krw_markets.append(market['market'])
 
-    driver.get(url)
-    # res = requests.get(url)
-    # page load
-    driver.implicitly_wait(1)
+    tickers = []
+    for market in krw_markets:
+        ticker = market.replace("KRW-", "")
+        tickers.append(ticker)
+    url2 = "https://api.upbit.com/v1/ticker"
+    params = {"markets": ','.join(krw_markets)}
+    response = requests.get(url2, params=params)
+    tickers_data = response.json()
 
-    # 스크롤 내리기 이동 전 위치
-    scroll_location = driver.execute_script("return document.body.scrollHeight")
+    volumes = []
+    for i, ticker in enumerate(tickers, start=1):
+        for ticker_data in tickers_data:
+            if ticker_data['market'].startswith(f'KRW-{ticker}'):
+                volumes.append({'ticker': ticker, 'volume': float(ticker_data['acc_trade_price'])})
 
-    dropdown = driver.find_element(By.XPATH, '//*[@id="__next"]/div/div[1]/div[2]/div/div[2]/div/div[2]/div')
+    top_volumes = sorted(volumes, key=lambda x: x['volume'], reverse=True)
 
-    dropdown.click()
-    driver.implicitly_wait(2)
+    for i, volume in enumerate(top_volumes):
+        volume['idx'] = i+1
+        print(f"{i + 1}. {volume['ticker']}: {volume['volume']}")
 
-    krw = driver.find_element(By.XPATH, '//*[@id="tippy-1"]/div/div[1]/div/div/button[2]')
-    krw.click()
-    driver.implicitly_wait(2)
-
-    while True:
-        try:
-            # 현재 스크롤의 가장 아래로 내림
-            driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
-
-            # 전체 스크롤이 늘어날 때까지 대기
-            time.sleep(1)
-
-            # 늘어난 스크롤 높이
-            scroll_height = driver.execute_script("return document.body.scrollHeight")
-            # print("sh", scroll_height)
-            # 늘어난 스크롤 위치와 이동 전 위치 같으면(더 이상 스크롤이 늘어나지 않으면) 종료
-            if scroll_location == scroll_height:
-                more_list = driver.find_element(By.XPATH,
-                                                '//*[@id="__next"]/div/div[1]/div[2]/div/div[3]/div[3]/button')
-                more_list.click()
-                time.sleep(3)
-
-            # 같지 않으면 스크롤 위치 값을 수정하여 같아질 때까지 반복
-            else:
-                # 스크롤 위치값을 수정
-                scroll_location = driver.execute_script("return document.body.scrollHeight")
-        except:
-            break
-
-    res = driver.page_source
-
-    bs = BeautifulSoup(res, 'html.parser')
-    selector = "tbody > tr > td"
-    columns = bs.select(selector)
-    # print(columns)
-    driver.close()
-    # ticker_list = [x.text.strip().replace('/', '-') for x in columns]
-    ticker_list = [x.text for x in columns]
+    # ticker_list = [x.text for x in top_volumes]
     # print(ticker_list)
 
-    newList = [ticker_list[i:i + 10] for i in range(0, len(ticker_list), 10)]
+    # newList = top_volumes[0:10]
     result = []
 
     if len(oldList) == 0:
-        oldList = newList
-        for i in newList:
-            result.append(printListout(i, i[0], i[6]))
+        oldList = top_volumes
+        for volume in top_volumes:
+            result.append(printListout(volume, volume['idx'], f"{volume['volume']}"))
 
     else:
-        result = compareList(oldList, newList)
+        result = compareList(oldList, top_volumes)
 
     print(result)
     return result
@@ -185,7 +152,7 @@ def scheduler():
      #timeList[currentIndex] = data
 
     # 매일 0시 실행
-    @sched.scheduled_job('cron', hour='15', id='test_0')
+    @sched.scheduled_job('cron', hour='3', id='test_0')
     def job0():
         global timeList1
         timeList1 = get_crolling()
@@ -193,7 +160,7 @@ def scheduler():
         print(timeList1)
 
     # 매일 4시 실행
-    @sched.scheduled_job('cron', hour='19',id='test_1')
+    @sched.scheduled_job('cron', hour='7',id='test_1')
     def job1():
         global timeList2
         timeList2 = get_crolling()
@@ -201,7 +168,7 @@ def scheduler():
         print(timeList2)
 
     # 매일 8시 실행
-    @sched.scheduled_job('cron', hour='23', id='test_2')
+    @sched.scheduled_job('cron', hour='11', id='test_2')
     def job2():
         global timeList3
         timeList3 = get_crolling()
@@ -210,7 +177,7 @@ def scheduler():
 
 
     # 매일 12시 실행
-    @sched.scheduled_job('cron', hour='3', id='test_3')
+    @sched.scheduled_job('cron', hour='15', id='test_3')
     def job3():
         global timeList4
         timeList4 = get_crolling()
@@ -219,7 +186,7 @@ def scheduler():
 
 
     # 매일 16시 실행
-    @sched.scheduled_job('cron', hour='7', id='test_4')
+    @sched.scheduled_job('cron', hour='19', id='test_4')
     def job4():
         global timeList5
         timeList5 = get_crolling()
@@ -228,7 +195,7 @@ def scheduler():
 
 
     # 매일 20시 실행
-    @sched.scheduled_job('cron', hour='11', id='test_5')
+    @sched.scheduled_job('cron', hour='23', id='test_5')
     def job5():
         global timeList6
         timeList6 = get_crolling()
@@ -241,28 +208,30 @@ def scheduler():
 
 @app.route('/') # 접속하는 url
 def index():
+    global top10
     t = datetime.now().hour
     print("hour:", t)
-    if 20 <= t:
+    if 23 <= t:
         data = timeList6
         prevData = timeList5
-    elif 16 <= t:
+    elif 19 <= t:
         data = timeList5
         prevData = timeList4
-    elif 12 <= t:
+    elif 15 <= t:
         data = timeList4
         prevData = timeList3
-    elif 8 <= t:
+    elif 11 <= t:
         data = timeList3
         prevData = timeList2
-    elif 4 <= t:
+    elif 7 <= t:
         data = timeList2
         prevData = timeList1
     else:
         data = timeList1
         prevData = timeList6
-
+    print("data", data)
+    print("top10", top10)
     return render_template('index.html', title="bitfind23", time=datetime.now(), data=data, prev=prevData, top10=top10)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run('0.0.0.0', port=8000, debug=True)
