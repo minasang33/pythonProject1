f957c49bdd0d2c6e2b6c06a739e59b2eb0be2f5e
hyeminwang <minasang33@gmail.com>
2023-02-20 22:24:48 +0900

    no crolling api change

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-import datetime as dt
+from datetime import datetime, timedelta
 import time
+import pytz
 from apscheduler.schedulers.background import BackgroundScheduler
 from flask import Flask, render_template, redirect, url_for  # Flask 라이브러리 선언
 app = Flask(__name__)
@@ -17,10 +18,14 @@ timeList5 = []
 timeList6 = []
 
 top10 = []
+# utc -> kst
+def convert_kst(dt_tm_utc):
+    #dt_tm_utc = datetime.strptime(utc_string, '%Y-%m-%d %H:%M:%S')
+    tm_kst = dt_tm_utc + timedelta(hours=9)
+    # str_datetime = tm_kst.strftime('%Y-%m-%d %H:%M:%S')
+    return tm_kst
 
 #스케줄 실행 코드
-def scheduler():
-    print("Scheduler is alive!")
 def printListout(copyArr, preCnt, prevValue):
     print('preCnt', preCnt, type(preCnt))
     pList = {
@@ -45,29 +50,19 @@ def compareList(oldList, newList):
                 fArr = []
                 bArr = []
 
-                if len(top10) == 0:
+                if len(top10) < 10:
                     top10.append(arr)
                 else:
-
-                    for x in top10:
+                    for k, x in enumerate(top10):
                         if between < 0:
-                            if x['bw'] > between:
-                                bArr.append(x.copy())
-                            else:
-                                fArr.append(x.copy())
-
-                    if len(fArr) == 0:
-                        fArr = [arr.copy()]
-                    else:
-                        fArr += [arr]
-
-                    if len(bArr) != 0:
-                        fArr += bArr
-                    top10 = fArr.copy()
+                            if x['bw'] < between:
+                                fArr = top10[0:k]
+                                fArr.append(arr)
+                                fArr.append(top10[k:all])
 
 
-                    if len(top10) > 10:
-                        top10.pop()
+                    if len(fArr) > 10:
+                        top10 = fArr[0:10]
                     print('top10', top10)
                 break
     return result
@@ -125,11 +120,8 @@ def get_crolling():
 
 def findData(data):
     global timeList1, timeList2, timeList3, timeList4, timeList5, timeList6
-
-    today = dt.datetime.now()
-    hour = dt.timedelta(hours=9)
-    t = today + hour
-    t = t.hour
+    today = convert_kst(datetime.now())
+    t = today.hour
     print("hour:", t)
     if 20 <= t:
         timeList6 = data
@@ -190,7 +182,6 @@ def scheduler():
     sched.add_job(job2, 'cron', hour='8')
     # 매일 12시 실행
     sched.add_job(job3, 'cron', hour='12')
-    sched.add_job(job3, 'cron', hour='13', minute='26')
     # 매일 16시 실행
     sched.add_job(job4, 'cron', hour='16')
     # 매일 20시 실행
@@ -202,34 +193,21 @@ def scheduler():
 
 @app.route('/') # 접속하는 url
 def index():
-    global top10
-    today = dt.datetime.now()
-    hour = dt.timedelta(hours=9)
-    t = today + hour
-    t = t.hour
-    print("hour:", t)
-    if 20 <= t:
-        data = timeList6
-        prevData = timeList5
-    elif 16 <= t:
-        data = timeList5
-        prevData = timeList4
-    elif 12 <= t:
-        data = timeList4
-        prevData = timeList3
-    elif 8 <= t:
-        data = timeList3
-        prevData = timeList2
-    elif 4 <= t:
-        data = timeList2
-        prevData = timeList1
-    else:
-        data = timeList1
-        prevData = timeList6
-
-    print("data", data)
-    print("top10", top10)
-    return render_template('index.html', title="bitfind23", time=dt.datetime.now(), data=data, prev=prevData, top10=top10)
+    global top10, timeList1, timeList2, timeList3, timeList4, timeList5, timeList6
+    dataList = []
+    dataList.append(timeList1)
+    dataList.append(timeList2)
+    dataList.append(timeList3)
+    dataList.append(timeList4)
+    dataList.append(timeList5)
+    dataList.append(timeList6)
+    return render_template('index.html',
+                           title="bitfind23",
+                           ktime=convert_kst(datetime.now()),
+                           time=datetime.now(),
+                           data=get_crolling(),
+                           top10=top10,
+                           dataList=dataList)
 
 if __name__ == '__main__':
     app.run()
