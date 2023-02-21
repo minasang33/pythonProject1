f921ee26239fc354607f3d5d257c99f833e247a6
hyeminwang <minasang33@gmail.com>
2023-02-21 23:24:04 +0900

    no crolling api change

--- app.py
+++ app.py
@@ -18,6 +18,35 @@ timeList5 = []
 timeList6 = []
 
 top10 = []
+#억단위로 표기
+def get_wonwha_string(num_wonwha_amout):
+    """ 입력된 원화를 4자리단위 한글로 변환한다 """
+    str_result = ""  # 결과문자열 초기화
+    str_sign = ""  # 부호 초기화
+    num_change = num_wonwha_amout  # 최초값을 모두 잔돈에 넣는다
+
+    if num_change == 0:  # 0원이면
+        str_result = "0"
+    elif num_change < 0:  # 음수이면
+        str_sign = "-"  # 음의 부호(Negative Sign)를 붙이고
+        num_change = abs(num_change)  # 절대값으로 변환 후 변환을 계속한다
+
+    if num_change >= 100000000:  # 1억 이상
+        str_result += f"{int(num_change // 100000000):,}억"
+        num_change = num_change % 100000000
+    # if num_change >= 10000:  # 1만 이상
+    #     str_result += f" {int(num_change // 10000):,}만"
+    #     num_change = num_change % 10000
+    # if num_change >= 1:  # 1 이상
+    #     str_result += f" {int(num_change):,}"
+
+    str_result = str_result.strip()  # Return a copy of the string with the leading and trailing characters removed
+    if len(str_result) >= 1:
+        return str_sign + str_result + "원"
+    else:
+        return str_result
+
+
 # utc -> kst
 def convert_kst(dt_tm_utc):
     #dt_tm_utc = datetime.strptime(utc_string, '%Y-%m-%d %H:%M:%S')
@@ -50,7 +79,7 @@ def compareList(oldList, newList):
                 fArr = []
                 bArr = []
 
-                if len(top10) < 10:
+                if len(top10) < 20:
                     top10.append(arr)
                 else:
                     for k, x in enumerate(top10):
@@ -61,8 +90,8 @@ def compareList(oldList, newList):
                                 fArr.append(top10[k:all])
 
 
-                    if len(fArr) > 10:
-                        top10 = fArr[0:10]
+                    if len(fArr) > 20:
+                        top10 = fArr[0:20]
 
                 break
 
@@ -103,6 +132,7 @@ def get_crolling():
 
     for i, volume in enumerate(top_volumes):
         volume['idx'] = i+1
+        volume['volume'] = get_wonwha_string(volume['volume'])
         print(f"{i + 1}. {volume['ticker']}: {volume['volume']}")
 
     result = []
@@ -193,6 +223,7 @@ def scheduler():
 
 @app.route('/') # 접속하는 url
 def index():
+
     global top10, timeList1, timeList2, timeList3, timeList4, timeList5, timeList6
     dataList = []
     dataList.append(timeList1)
