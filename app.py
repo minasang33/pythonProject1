f039455fa1a3030d9d0a3c8ebeed3e63f5ddb566
hyeminwang <minasang33@gmail.com>
2023-02-22 14:36:26 +0900

    no crolling api change

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 import requests
 
 url ="https://coinmarketcap.com/exchanges/upbit/"
-oldList =[]
+
 
 timeList1 = []
 timeList2 = []
@@ -17,7 +17,7 @@ timeList4 = []
 timeList5 = []
 timeList6 = []
 
-top10 = []
+top20 = []
 #억단위로 표기
 def get_wonwha_string(num_wonwha_amout):
     """ 입력된 원화를 4자리단위 한글로 변환한다 """
@@ -34,11 +34,11 @@ def get_wonwha_string(num_wonwha_amout):
     if num_change >= 100000000:  # 1억 이상
         str_result += f"{int(num_change // 100000000):,}억"
         num_change = num_change % 100000000
-    # if num_change >= 10000:  # 1만 이상
-    #     str_result += f" {int(num_change // 10000):,}만"
-    #     num_change = num_change % 10000
-    # if num_change >= 1:  # 1 이상
-    #     str_result += f" {int(num_change):,}"
+    elif num_change >= 10000:  # 1만 이상
+        str_result += f" {int(num_change // 10000):,}만"
+        num_change = num_change % 10000
+    elif num_change >= 1:  # 1 이상
+        str_result += f" {int(num_change):,}"
 
     str_result = str_result.strip()  # Return a copy of the string with the leading and trailing characters removed
     if len(str_result) >= 1:
@@ -61,6 +61,7 @@ def printListout(copyArr, preCnt, prevValue):
         "id": copyArr['idx'],
         "ticker": copyArr['ticker'],
         "change": str(preCnt)+"->"+str(copyArr['idx']),
+        "value": copyArr['volume'],
         "prevValue": prevValue,
         "bw": preCnt - copyArr['idx']
     }
@@ -68,42 +69,35 @@ def printListout(copyArr, preCnt, prevValue):
 
 
 def compareList(oldList, newList):
-    global top10
     result = []
     for i, n in enumerate(newList):
         for j, o in enumerate(oldList):
             if n['ticker'] == o['ticker']: #ticker 가 같은지 비교
-                arr = printListout(n, o['idx'], o['volume'])
+                arr = printListout(n, o['id'], o['prevValue'])
                 result.append(arr)
-                between = i - j
-                fArr = []
-                bArr = []
-
-                if len(top10) < 20:
-                    top10.append(arr)
-                else:
-                    for k, x in enumerate(top10):
-                        if between < 0:
-                            if x['bw'] < between:
-                                fArr = top10[0:k]
-                                fArr.append(arr)
-                                fArr.append(top10[k:all])
-
-
-                    if len(fArr) > 20:
-                        top10 = fArr[0:20]
-
-                break
-
-    for top in enumerate(top10):
-        print('top10===')
-        print(top)
-
     return result
 
 def get_crolling():
     print('get_crolling start')
-    global oldList
+    oldList = []
+    today = convert_kst(datetime.now())
+    t = today.hour
+    print("hour:", t)
+    if 20 <= t-4:
+        oldList = timeList5
+    elif 16 <= t-4:
+        oldList = timeList4
+    elif 12 <= t-4:
+        oldList = timeList3
+    elif 8 <= t-4:
+        oldList = timeList2
+    elif 4 <= t-4:
+        oldList = timeList1
+    else:
+        oldList = timeList6
+    print("oldList=============================")
+    print(oldList)
+
     url1 = "https://api.upbit.com/v1/market/all"
     response = requests.get(url1)
     markets = response.json()
@@ -166,34 +160,56 @@ def findData(data):
     else:
         timeList1 = data
 
+def getTop20(oldList, newList):
+    result = []
+    for i, n in enumerate(newList):
+        for j, o in enumerate(oldList):
+            if n['ticker'] == o['ticker']:  # ticker 가 같은지 비교
+                arr = {
+                    "id": n['id'],
+                    "ticker": n['ticker'],
+                    "change": str(j)+"->"+str(n['id']),
+                    "value": n['value'],
+                    "prevValue": o['value'],
+                    "bw": j - n['id']
+                }
+                result.append(arr)
+    return result
+
 def job0():
-    global timeList1
+    global timeList1, top20
     timeList1 = get_crolling()
+    top20 = getTop20(timeList6, timeList1)
     print(f'job0 : {time.strftime("%H:%M:%S")}')
     print(timeList1)
 def job1():
-    global timeList2
+    global timeList2, top20
     timeList2 = get_crolling()
+    top20 = getTop20(timeList6, timeList1)
     print(f'job1 : {time.strftime("%H:%M:%S")}')
     print(timeList2)
 def job2():
-    global timeList3
+    global timeList3, top20
     timeList3 = get_crolling()
+    top20 = getTop20(timeList2, timeList3)
     print(f'job2 : {time.strftime("%H:%M:%S")}')
     print(timeList3)
 def job3():
-    global timeList4
+    global timeList4, top20
     timeList4 = get_crolling()
+    top20 = getTop20(timeList3, timeList4)
     print(f'job3 : {time.strftime("%H:%M:%S")}')
     print(timeList4)
 def job4():
-    global timeList5
+    global timeList5, top20
     timeList5 = get_crolling()
+    top20 = getTop20(timeList4, timeList5)
     print(f'job4 : {time.strftime("%H:%M:%S")}')
     print(timeList5)
 def job5():
-    global timeList6
+    global timeList6, top20
     timeList6 = get_crolling()
+    top20 = getTop20(timeList5, timeList6)
     print(f'job5 : {time.strftime("%H:%M:%S")}')
     print(timeList6)
 @app.route('/scheduler')
@@ -204,18 +220,25 @@ def scheduler():
 
     sched.remove_all_jobs()
 
-    # 매일 0시 실행
-    sched.add_job(job0, 'cron', hour='0')
-    # 매일 4시 실행
-    sched.add_job(job1, 'cron', hour='4')
-    # 매일 8시 실행
-    sched.add_job(job2, 'cron', hour='8')
-    # 매일 12시 실행
-    sched.add_job(job3, 'cron', hour='12')
-    # 매일 16시 실행
-    sched.add_job(job4, 'cron', hour='16')
-    # 매일 20시 실행
-    sched.add_job(job5, 'cron', hour='20')
+    # # 매일 0시 실행
+    # sched.add_job(job0, 'cron', hour='0')
+    # # 매일 4시 실행
+    # sched.add_job(job1, 'cron', hour='4')
+    # # 매일 8시 실행
+    # sched.add_job(job2, 'cron', hour='8')
+    # # 매일 12시 실행
+    # sched.add_job(job3, 'cron', hour='12')
+    # # 매일 16시 실행
+    # sched.add_job(job4, 'cron', hour='16')
+    # # 매일 20시 실행
+    # sched.add_job(job5, 'cron', hour='20')
+
+    sched.add_job(job0, 'cron', hour='15')
+    sched.add_job(job1, 'cron', hour='15', minute='10')
+    sched.add_job(job2, 'cron', hour='15', minute='20')
+    sched.add_job(job3, 'cron', hour='15', minute='30')
+    sched.add_job(job4, 'cron', hour='15', minute='40')
+    sched.add_job(job5, 'cron', hour='15', minute='50')
 
     sched.start()
     print("scheduler start")
@@ -224,7 +247,7 @@ def scheduler():
 @app.route('/') # 접속하는 url
 def index():
 
-    global top10, timeList1, timeList2, timeList3, timeList4, timeList5, timeList6
+    global top20, timeList1, timeList2, timeList3, timeList4, timeList5, timeList6
     dataList = []
     dataList.append(timeList1)
     dataList.append(timeList2)
@@ -237,7 +260,7 @@ def index():
                            ktime=convert_kst(datetime.now()),
                            time=datetime.now(),
                            data=get_crolling(),
-                           top10=top10,
+                           # top10=top20,
                            dataList=dataList)
 
 if __name__ == '__main__':
